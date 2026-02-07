6a0436fef766f7ecc8bb9e4aafa0f8812bfb9222
cest@Eva <evangelinenirere@gmail.com>
2026-02-07 18:24:04 +0200

    Add files via upload

---
+++ for.js
@@ -0,0 +1,10 @@
+//for loop
+for(let i=0; i<= 5; i++){
+    console.log(`itearation number:${i}`);
+}
+console.log("-----");
+//countdown using for loop
+let j;
+for(j =10; j >=0; j--) {
+    console.log(`countdown: ${j}`);
+}
